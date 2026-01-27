cd1204c7051157eda8d9f2b31f3bde9e27b7e01c
rutujasontakke26 <sontakkerutuja55@gmail.com>
2026-01-27 16:12:38 +0530

    theme togglers added

---
+++ js/index.js
@@ -0,0 +1,11 @@
+function toggleTheme() {
+    document.body.classList.toggle("dark");
+
+    var btn = document.getElementById("themeBtn");
+
+    if (document.body.classList.contains("dark")) {
+      btn.innerHTML = "☀️ Light";
+    } else {
+      btn.innerHTML = "🌙 Dark";
+    }
+  }
